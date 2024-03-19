8a59740846b405c0f5cb8a1a73afc4136a8bc8e9
Aram Eloyan <aeloyan@c1r1s1.42yerevan.am>
2024-03-19 16:13:28 +0400

    adf

--- main.cpp
+++ main.cpp
@@ -13,33 +13,11 @@ int main()
 	std::vector<Config>::iterator conf = configs.begin();
 	while(conf != configs.end())
 	{
-		int pid = fork();
-		if (pid == -1)
-			exit(3);
-		else if (pid == 0)
 		{
 			Server(*conf);
 		}
 		else
 			++conf;
-	}
-	while (true)
-	{
-		pid_t terminated_pid = waitpid(-1, &status, 0);
-		if (terminated_pid == -1)
-		{
-			if (errno == ECHILD)
-				// No more child processes left
-				break;
-			else
-			{
-				// Error handling
-				std::cerr << "Error while waiting for child processes: " << strerror(errno) << std::endl;
-				return 1;
-			}
-		}
-		else
-			std::cout << "Child process " << terminated_pid << " has terminated" << std::endl;
 	}
 	return 0;
 }
