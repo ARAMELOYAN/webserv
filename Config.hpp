f4036ac6bf96820953bb6a4065a07bc0c3472ce2
Aram Eloyan <aeloyan@c1r1s1.42yerevan.am>
2024-03-27 21:19:06 +0400

    petq e server@ sarqel mi qani porteri het ev vor bolor clientneric requesty karda buffsizei chapov ev pahi request classi mej

--- Config.hpp
+++ Config.hpp
@@ -12,16 +12,6 @@ class Config
 			_sa.sin_family = AF_INET;
 			_sa.sin_addr.s_addr = htonl(ip);
 			_sa.sin_port = htons(port);
-			_socket = socket(_sa.sin_family, SOCK_STREAM, 0);
-			if (_socket == -1)
-				std::cout << "Socket error: " << strerror(errno) << "\n";
-			std::cout << "Created server socket fd: " << _socket << "\n";
-			if (bind(_socket, (struct sockaddr *)&_sa, sizeof _sa) != 0)
-				std::cout << "Bind error: " << strerror(errno) << "\n";
-			std::cout << "Bound socket to localhost port " << getPort() << "\n";
-			std::cout << "Listening on port " << getPort()<< "\n";
-			if (listen(_socket, 10) != 0)
-				std::cout << "Listen error: " << strerror(errno) << "\n";
 		}
 		~Config()
 		{
@@ -30,4 +20,5 @@ class Config
 		unsigned short getPort() const		{return ntohs(_sa.sin_port);}
 		const sockaddr_in& getSa() const	{return _sa;}
 		int getSockId() const				{return _socket;}
+		void setSockId(int fd)				{_socket = fd;}
 };
--- Webserv.cpp
+++ Webserv.cpp
@@ -39,17 +39,31 @@ const char* Webserv::Socket_error::what() const throw()
 
 void Webserv::socket_create()
 {
+	std::vector<Config>::iterator server = _server.begin();
+	while (server != _server.end())
+	{
+		server->setSockId(socket(AF_INET, SOCK_STREAM, 0));
+		if (server->getSockId() == -1)
+			throw  Socket_error(std::string("Socket error: ") + strerror(errno));
+		std::cout << "Created server socket fd: " << server->getSockId() << "\n";
+		if (bind(server->getSockId(), (struct sockaddr *)&server->getSa(), sizeof server->getSa()) != 0)
+			throw Socket_error(std::string("Bind error: ") + strerror(errno));
+		std::cout << "Bound socket to localhost port " << server->getPort() << "\n";
+		std::cout << "Listening on port " << server->getPort()<< "\n";
+		if (listen(server->getSockId(), 10) != 0)
+			throw Socket_error(std::string("Listen error: ") + strerror(errno));
+	}
 }
 
 void Webserv::accept_connection()
 {
 	int client_fd = accept(_server.begin()->getSockId(), NULL, NULL);
 	if (client_fd == -1)
-		throw Socket_error(std::string("[Webserv] Accept error: ") + strerror(errno));
+		throw Socket_error(std::string("Accept error: ") + strerror(errno));
 	FD_SET(client_fd, &_all_sockets);
 	if (client_fd > fd_max)
 		fd_max = client_fd;
-	std::cout << "[Webserv] Accepted new connection on client socket " << client_fd << ".\n";
+	std::cout << "Accepted new connection on client socket " << client_fd << ".\n";
 	//request(client_fd);
 	responce(client_fd);
 	close(client_fd);
@@ -61,5 +75,5 @@ void Webserv::responce(int sock)
 	memset(&_responce_msg, '\0', sizeof _responce_msg);
 	sprintf(_responce_msg, "HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length: 1234\nConnection: close\n\n<!DOCTYPE html>\n<html>\n<head>\n<title>HELLO HTML</title>\n</head>\n<body>\n<h1>Hello Html from my server</h1>\n</body>\n</html>");
 	if (send(sock, _responce_msg, strlen(_responce_msg), 0) == -1)
-		throw Socket_error(std::string("[Webserv] Send error to client ") + "client_fd" + ": " + strerror(errno));
+		throw Socket_error(std::string("Send error to client ") + "client_fd" + ": " + strerror(errno));
 }
