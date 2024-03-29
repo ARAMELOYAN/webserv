dfa9fdf9ef6bbd99c2e99231f39437b589d6291d
Aram Eloyan <aeloyan@c1r1s1.42yerevan.am>
2024-03-29 13:42:18 +0400

    asdddd

--- Webserv.cpp
+++ Webserv.cpp
@@ -9,8 +9,6 @@ Webserv::Webserv(std::vector<Config> &serv): _servers(serv)
 	std::cout << "WEBSERV\n";
 	socket_create();
 	int fd_max = _servers.begin()->getSockId();
-	std::cout << fd_max << "\n";
-
 	while (1)
 	{
 		_read_fds = _all_sockets;
